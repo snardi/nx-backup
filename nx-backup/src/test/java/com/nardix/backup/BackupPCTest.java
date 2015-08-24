b1f71547510a501d0c8951751f23c4abfbe6a4d1
Sergio Nardi Spinelli <sergio.nardi@gmail.com>
2015-08-23 22:23:37 -0300

    Signed-off-by: Sergio Nardi Spinelli <sergio.nardi@gmail.com>

--- nx-backup/src/test/java/com/nardix/backup/BackupPCTest.java
+++ nx-backup/src/test/java/com/nardix/backup/BackupPCTest.java
@@ -1,5 +1,8 @@
 package com.nardix.backup;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.SortedSet;
@@ -33,7 +36,7 @@ public class BackupPCTest {
 	public void testFindDuplicates() throws Exception {
 		String backupRepo = "C:/snardi/git-nx-backup/nx-backup/data/repoDir";
 		//String sourceDir = "C:/snardi/git-nx-backup/nx-backup/data/sourceDir";
-		String sourceDir = "C:/snardi/orga/TPA";
+		String sourceDir = "C:/snardi";
 		BackupPC backup = new BackupPC(backupRepo, sourceDir);
 		FindDuplicatesFileVisitor f = backup.findDuplicates();
 		
@@ -45,11 +48,17 @@ public class BackupPCTest {
 		System.out.println("############################################");
 		System.out.println("Duplicated directories");
 		TreeMap<String, List<String>> dupDirs = f.getDuplicateDirs();
-		for (Entry<String, List<String>> entry: dupDirs.entrySet()) {
-			System.out.println(entry.getKey());
+		
+		try (FileWriter ff = new FileWriter(new File("C:/snardi/tmp/out.txt"));
+				BufferedWriter bw = new BufferedWriter(ff)) {
+			for (Entry<String, List<String>> entry: dupDirs.entrySet()) {
+				System.out.println(entry.getKey());
+				bw.write(entry.getKey());
 				for (String file: entry.getValue()) {
 					System.out.println("\t\t" + file);
+					bw.write("\t\t" + file);
 				}
+			}
 		}
 		
 	}
